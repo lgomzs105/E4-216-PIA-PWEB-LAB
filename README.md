22789e91aacae1c7a486cc185b8025f61f5b4fee
Luka <lgomzs.105@gmail.com>
2025-05-18 18:54:54 -0600

    Inicializando el proyecto con la ruta inicial, y los datos necesarios del package.json y el app.js

---
+++ README.md
@@ -0,0 +1,2 @@
+# LabProWEB-PIA
+Proyecto para mi laboratorio de programación web 2043635
---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Pagina inicial');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto http://localhost:${port}/`);
+});
